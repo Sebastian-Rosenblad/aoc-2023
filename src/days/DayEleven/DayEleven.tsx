96e92cd5cfa488f4e54c2b06dd70df47d90ad332
Sebastian-Rosenblad <sebastian.a.rosenblad@gmail.com>
2023-12-11 06:58:26 +0100

    Day 11 part 2

--- src/days/DayEleven/DayEleven.tsx
+++ src/days/DayEleven/DayEleven.tsx
@@ -11,48 +11,45 @@ function DayEleven() {
   const exampleData: Array<string> = dayElevenExample.split(/\r?\n/);
 
   /**
-   * Part 1: time 00:00:00 - rank 0000
-   * Part 2: time 00:00:00 - rank 0000
+   * Part 1: time 00:29:07 - rank 3673
+   * Part 2: time 00:52:13 - rank 4526
    */
 
   function calculate(a: Array<string>, partOne: boolean): string {
-    if (partOne) {
-      let space = expandSpace(a);
-      const galaxies: Array<CoordsM> = space.map((line, i) => findGalaxies(line).map(j => {
-        return { x: j, y: i }
-      })).flat();
-      let distances: Array<number> = [];
-      for (let i = 0; i < galaxies.length; i++) {
-        for (let j = i + 1; j < galaxies.length; j++) {
-          distances.push(distance(galaxies[i], galaxies[j]));
-        }
+    let galaxies: Array<CoordsM> = a.map((line, i) => findGalaxies(line).map(j => {
+      return { x: j, y: i }
+    })).flat();
+    const emptyspace = {
+      x: new Array(a[0].length).fill(-1).map((n, i) => columnHasGalaxy(a, i) ? -1 : i).filter(n => n >= 0),
+      y: a.map((line, i) => line.includes("#") ? -1 : i).filter(n => n >= 0)
+    };
+    let distances: Array<number> = [];
+    for (let i = 0; i < galaxies.length; i++) {
+      for (let j = i + 1; j < galaxies.length; j++) {
+        if (partOne) distances.push(distance(galaxies[i], galaxies[j], emptyspace, 1));
+        else distances.push(distance(galaxies[i], galaxies[j], emptyspace, 999999));
       }
-      return distances.reduce((a, b) => a + b, 0).toString();
     }
-    return "";
+    return distances.reduce((a, b) => a + b, 0).toString();
   }
   function findGalaxies(line: string): Array<number> {
     let galaxies: Array<number> = [];
     for (let i = 0; i < line.length; i++) if (line[i] === "#") galaxies.push(i);
     return galaxies;
   }
-  function expandSpace(space: Array<string>): Array<string> {
-    for (let i = 0; i < space.length; i++) if (!space[i].includes("#"))
-      space.splice(i++, 0, new Array<string>(space[0].length).fill(".").reduce((a, b) => a + b, ""));
-    for (let i = 0; i < space[0].length; i++) {
-      if (!columnHasGalaxy(space, i)) {
-        for (let j = 0; j < space.length; j++) space[j] = space[j].slice(0, i) + "." + space[j].slice(i);
-        i += 1;
-      }
-    }
-    return space;
-  }
   function columnHasGalaxy(space: Array<string>, column: number): boolean {
     for (let i = 0; i < space.length; i++) if (space[i][column] === "#") return true;
     return false;
   }
-  function distance(a: CoordsM, b: CoordsM): number {
-    return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+  function distance(a: CoordsM, b: CoordsM, emptyspace: { x: Array<number>, y: Array<number> }, expanded: number): number {
+    let xb: number = emptyspace.x.filter(cx => inbetween(cx, a.x, b.x)).length * expanded;
+    let yb: number = emptyspace.y.filter(cy => inbetween(cy, a.y, b.y)).length * expanded;
+    return Math.abs(a.x - b.x) + Math.abs(a.y - b.y) + xb + yb;
+  }
+  function inbetween(v: number, a: number, b: number): boolean {
+    if (a > b && v > b && v < a) return true;
+    if (b > a && v > a && v < b) return true;
+    return false;
   }
 
   return (
