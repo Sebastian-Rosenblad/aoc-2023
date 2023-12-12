96a11c885309e9b97a23dc765e016d24e2a2480c
Sebastian-Rosenblad <sebastian.a.rosenblad@gmail.com>
2023-12-12 06:51:50 +0100

    Day 12 part 1

--- src/days/DayTwelve/DayTwelve.tsx
+++ src/days/DayTwelve/DayTwelve.tsx
@@ -1,6 +1,11 @@
 import { dayTwelveData } from './data';
 import { dayTwelveExample } from './example_data';
 
+interface SpringM {
+  damaged: Array<number>;
+  condition: Array<number>;
+}
+
 function DayTwelve() {
   const data: Array<string> = dayTwelveData.split(/\r?\n/);
   const exampleData: Array<string> = dayTwelveExample.split(/\r?\n/);
@@ -11,9 +16,41 @@ function DayTwelve() {
    */
 
   function calculate(a: Array<string>, partOne: boolean): string {
-    if (partOne)
-      return "";
+    if (partOne) {
+      const springs: Array<SpringM> = a.map(line => {
+        return {
+          damaged: line.split(" ")[1].split(",").map(val => parseInt(val)),
+          condition: line.split(" ")[0].split("").map(val => val === "?" ? -1 : (val === "." ? 0 : 1))
+        };
+      });
+      return springs.map(spring => arrangements(spring)).reduce((a, b) => a + b, 0).toString();
+    }
     return "";
+  }
+  function arrangements(spring: SpringM): number {
+    return arrange([], spring, 0).filter(pos => pos).length;
+  }
+  function arrange(condition: Array<number>, spring: SpringM, index: number): Array<boolean> {
+    if (condition.length === spring.condition.length)
+      return [possible(condition, spring.damaged)];
+    if (spring.condition[index] >= 0)
+      return arrange([...condition, spring.condition[index]], spring, index + 1);
+    return [...arrange([...condition, 0], spring, index + 1), ...arrange([...condition, 1], spring, index + 1)];
+  }
+  function possible(condition: Array<number>, damaged: Array<number>): boolean {
+    let pairs: Array<number> = [0];
+    for (let i = 0; i < condition.length; i++) {
+      if (condition[i] === 0) pairs.push(0);
+      else pairs[pairs.length - 1] += 1;
+    }
+    return equals(pairs.filter(p => p > 0), damaged);
+  }
+  function equals(a: Array<number>, b: Array<number>): boolean {
+    if (a.length === b.length) {
+      for (let i = 0; i < a.length; i++) if (a[i] !== b[i]) return false;
+      return true;
+    }
+    return false;
   }
 
   return (
@@ -29,3 +66,4 @@ function DayTwelve() {
 }
 
 export default DayTwelve;
+//
