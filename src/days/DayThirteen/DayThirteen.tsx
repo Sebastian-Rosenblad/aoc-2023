ce6f5030fda3019a9dee8cc89edfd76f70742c32
Sebastian-Rosenblad <sebastian.a.rosenblad@gmail.com>
2023-12-13 07:09:53 +0100

    Day 13

--- src/days/DayThirteen/DayThirteen.tsx
+++ src/days/DayThirteen/DayThirteen.tsx
@@ -6,14 +6,43 @@ function DayThirteen() {
   const exampleData: Array<string> = dayThirteenExample.split(/\r?\n/);
 
   /**
-   * Part 1: time 00:00:00 - rank 0000
-   * Part 2: time 00:00:00 - rank 0000
+   * Part 1: time 00:23:07 - rank 1439
+   * Part 2: time 00:35:43 - rank 1326
    */
 
   function calculate(a: Array<string>, partOne: boolean): string {
+    const patterns: Array<Array<string>> = [[]];
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] === "") patterns.push([]);
+      else patterns[patterns.length - 1].push(a[i]);
+    }
     if (partOne)
-      return "";
-    return "";
+      return patterns.map(pattern => reflectionValue(pattern, 0)).reduce((a, b) => a + b, 0).toString();
+    return patterns.map(pattern => reflectionValue(pattern, 1)).reduce((a, b) => a + b, 0).toString();
+  }
+  function reflectionValue(pattern: Array<string>, errors: number): number {
+    let mirrored: Array<string> = new Array(pattern[0].length).fill("");
+    for (let i = 0; i < pattern.length; i++) for (let j = 0; j < mirrored.length; j++) mirrored[j] += pattern[i][j];
+    return reflectionPoint(pattern, errors) * 100 + reflectionPoint(mirrored, errors);
+  }
+  function reflectionPoint(pattern: Array<string>, errors: number): number {
+    for (let i = 0.5; i < pattern.length - 1; i += 0.5) {
+      if (mirrors(pattern, Math.floor(i), Math.floor(i + 1), 0, errors))
+        return Math.floor(i + 1);
+    }
+    return 0;
+  }
+  function mirrors(pattern: Array<string>, low: number, high: number, errors: number, maxErrors: number): boolean {
+    if (low < 0 || high >= pattern.length) return errors === maxErrors;
+    if (pattern[low] === pattern[high]) return mirrors(pattern, low - 1, high + 1, errors, maxErrors);
+    let totalErrors = errors += differences(pattern[low], pattern[high]);
+    if (totalErrors > maxErrors) return false;
+    return mirrors(pattern, low - 1, high + 1, totalErrors, maxErrors);
+  }
+  function differences(a: string, b: string): number {
+    let d: number = 0;
+    for (let i = 0; i < a.length; i++) if (a[i] !== b[i]) d += 1;
+    return d;
   }
 
   return (
