d9463204a2ee66071e7fb0d8daf13a343ffe9a1b
Sebastian-Rosenblad <sebastian.a.rosenblad@gmail.com>
2023-12-15 06:51:18 +0100

    Day 15

--- src/days/DayFifteen/DayFifteen.tsx
+++ src/days/DayFifteen/DayFifteen.tsx
@@ -1,19 +1,56 @@
 import { dayFifteenData } from './data';
 import { dayFifteenExample } from './example_data';
 
+interface DataM {
+  box: number;
+  label: string;
+  focal: number;
+}
+
 function DayFifteen() {
   const data: Array<string> = dayFifteenData.split(/\r?\n/);
   const exampleData: Array<string> = dayFifteenExample.split(/\r?\n/);
 
   /**
-   * Part 1: time 00:00:00 - rank 0000
-   * Part 2: time 00:00:00 - rank 0000
+   * Part 1: time 00:05:53 - rank 1673
+   * Part 2: time 00:47:16 - rank 4045
    */
 
   function calculate(a: Array<string>, partOne: boolean): string {
+    let steps: Array<string> = a[0].split(",");
     if (partOne)
-      return "";
-    return "";
+      return steps.map(step => hash(step)).reduce((a, b) => a + b, 0).toString();
+    let hashmap: Array<DataM> = a[0].split(",").map(step => {
+      let l: string = step.includes("=") ? step.split("=")[0] : step.split("-")[0];
+      return {
+        box: hash(l),
+        label: l,
+        focal: step.includes("=") ? parseInt(step.split("=")[1]) : -1
+      };
+    });
+    return boxes(hashmap).map((box, i) => box.map((slot, i) => slot.focal * (i + 1)).reduce((a, b) => a + b, 0) * (i + 1)).reduce((a, b) => a + b, 0).toString();
+  }
+  function hash(chars: string): number {
+    let current: number = 0;
+    chars.split("").forEach(char => {
+      current += char.charCodeAt(0);
+      current *= 17;
+      current %= 256;
+    });
+    return current;
+  }
+  function boxes(hashmap: Array<DataM>): Array<Array<DataM>> {
+    let boxes: Array<Array<DataM>> = new Array(256).fill(0).map(() => []);
+    hashmap.forEach(h => {
+      let box: number = hash(h.label);
+      let index: number = boxes[box].findIndex(b => b.label === h.label);
+      if (h.focal < 0) if (index >= 0) boxes[box].splice(index, 1);
+      else {
+        if (index >= 0) boxes[box][index] = h;
+        else boxes[box].push(h);
+      }
+    });
+    return boxes;
   }
 
   return (
--- src/days/DayFifteen/example_data.ts
+++ src/days/DayFifteen/example_data.ts
@@ -1,1 +1,1 @@
-export const dayFifteenExample:string=``;
+export const dayFifteenExample:string=`rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`;
