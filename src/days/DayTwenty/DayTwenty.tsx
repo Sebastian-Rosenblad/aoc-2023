0e94d1c9af9ac0866dcdbbbb8e49121f89ec42d4
Sebastian-Rosenblad <sebastian.a.rosenblad@gmail.com>
2024-01-02 11:36:54 +0100

    Day 20 part 2

--- src/days/DayTwenty/DayTwenty.tsx
+++ src/days/DayTwenty/DayTwenty.tsx
@@ -13,6 +13,10 @@ interface PulseM {
   to: string;
   high: boolean;
 }
+interface ConjunctionM {
+  label: string;
+  value: number;
+}
 
 function DayTwenty() {
   const data: Array<string> = dayTwentyData.split(/\r?\n/);
@@ -28,9 +32,7 @@ function DayTwenty() {
     let modules: Array<ModuleM> = Parse(a);
     dictionary = {};
     modules.forEach(module => dictionary[module.label] = module);
-    console.log(modules);
-    if (partOne) return PressButton(modules, 1000) + " (" + (Date.now() - time) + ")";
-    return "";
+    return PressButton(modules, 1000, partOne) + " (" + (Date.now() - time) + ")";
   }
   function Parse(a: Array<string>): Array<ModuleM> {
     let modules: Array<ModuleM> = a.map(line => {
@@ -46,24 +48,23 @@ function DayTwenty() {
       return module;
     });
     modules.filter(module => module.type === '&').forEach(module => {
-      module.inputs = modules.filter(m => m.destinations.includes(module.label)).map(m => { return { from: '', to: m.label, high: false }; })
+      module.inputs = modules.filter(m => m.destinations.includes(module.label)).map(m => {
+        return { from: m.label, to: '', high: false };
+      });
     });
     return modules;
   }
-  function PressButton(modules: Array<ModuleM>, amout: number): number {
+  function PressButton(modules: Array<ModuleM>, amout: number, partOne: boolean): number {
     let cycles: Array<{ key: string; low: number; high: number; }> = [];
+    let flops: Array<ConjunctionM> = FindFlops(modules, "rx");
     do {
-      cycles.push({
-        key: ModulesKey(modules), low: 0, high: 0
-      });
-      console.log(cycles[cycles.length - 1].key);
+      cycles.push({ key: ModulesKey(modules), low: 0, high: 0 });
       let total: Array<number> = [0, 0];
       let pulses: Array<PulseM> = [{ from: 'button', to: 'broadcaster', high: false }];
       while (pulses.length > 0) {
         const pulse: PulseM | undefined = pulses.shift();
         if (pulse !== undefined) {
           total[pulse.high ? 1 : 0] += 1;
-          //console.log(pulse);
           let module: ModuleM = dictionary[pulse.to];
           if (module) {
             if (module.type === "%") {
@@ -74,9 +75,8 @@ function DayTwenty() {
             }
             else if (module.type === "&") {
               module.inputs.forEach(input => {
-                if (input.to === pulse.from) input.high = pulse.high;
+                if (input.from === pulse.from) input.high = pulse.high;
               });
-              //console.log(JSON.stringify(module.inputs));
               if (module.inputs.filter(input => !input.high).length === 0)
                 module.destinations.forEach(destination => pulses.push({ from: module.label, to: destination, high: false }));
               else module.destinations.forEach(destination => pulses.push({ from: module.label, to: destination, high: true }));
@@ -84,15 +84,25 @@ function DayTwenty() {
             else {
               module.destinations.forEach(destination => pulses.push({ from: module.label, to: destination, high: pulse.high }));
             }
+            if (!partOne && !pulse.high && flops.find(flop => flop.label === pulse.to)) {
+              modules.filter(module => module.label === pulse.to).forEach(module => {
+                if (!module.state) {
+                  flops = flops.map(flop => flop.label === pulse.to && flop.value < 0 ? { label: flop.label, value: cycles.length } : flop);
+                }
+              });
+              if (!flops.find(flop => flop.value < 0)) return flops.map(flop => flop.value).reduce((a, b) => a * b, 1);
+            }
           }
         }
       }
-      cycles[cycles.length - 1].low = total[0];
-      cycles[cycles.length - 1].high = total[1];
-    } while (cycles.findIndex(cycle => cycle.key === ModulesKey(modules)) < 0 && cycles.length < amout);
+      if (partOne) {
+        cycles[cycles.length - 1].low = total[0];
+        cycles[cycles.length - 1].high = total[1];
+      }
+    } while (cycles.findIndex(cycle => cycle.key === ModulesKey(modules)) < 0 && (cycles.length < amout || !partOne));
     let total: Array<number> = [0, 0];
     const min: number = Math.floor(1000 / cycles.length);
-    cycles.forEach((cycle, i) => {
+    cycles.forEach(cycle => {
       total[0] += cycle.low * min;
       total[1] += cycle.high * min;
     });
@@ -100,6 +110,12 @@ function DayTwenty() {
   }
   function ModulesKey(modules: Array<ModuleM>): string {
     return modules.map(module => module.type === "&" ? module.inputs.map(input => input.high ? "H" : "L").join("") : (module.state ? "F" : "T")).join("");
+  }
+  function FindFlops(modules: Array<ModuleM>, destination: string): Array<ConjunctionM> {
+    let conjunction: string = modules.find(module => module.destinations.includes(destination))?.label || "";
+    return modules.filter(module => module.destinations.includes(conjunction)).map(module => {
+      return { label: module.label, value: -1 };
+    });
   }
 
   return (
@@ -110,12 +126,8 @@ function DayTwenty() {
       <h2>Example: {calculate(dayTwentyExampleTwo.split(/\r?\n/), true)}</h2>
       <p>Part 2</p>
       <h1>Answer: {calculate(data, false)}</h1>
-      <h2>Example: {calculate(dayTwentyExampleOne.split(/\r?\n/), false)}</h2>
     </div>
   );
 }
 
 export default DayTwenty;
-/**
- * 
- */
