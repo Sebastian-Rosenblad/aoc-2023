4b5053061bd2dcedce3d6f53db82937633705a59
Sebastian-Rosenblad <sebastian.a.rosenblad@gmail.com>
2024-01-02 11:42:41 +0100

    Init 21

---
+++ src/days/DayTwentyone/DayTwentyone.tsx
@@ -0,0 +1,31 @@
+import { dayTwentyoneData } from './data';
+import { dayTwentyoneExample } from './example_data';
+
+function DayTwentyone() {
+  const data: Array<string> = dayTwentyoneData.split(/\r?\n/);
+  const exampleData: Array<string> = dayTwentyoneExample.split(/\r?\n/);
+
+  /**
+   * Part 1: time 00:00:00 - rank 0000
+   * Part 2: time 00:00:00 - rank 0000
+   */
+
+  function calculate(a: Array<string>, partOne: boolean): string {
+    if (partOne)
+      return "";
+    return "";
+  }
+
+  return (
+    <div className='day-twentyone'>
+      <p>Day twentyone: Part 1</p>
+      <h1>Answer: {calculate(data, true)}</h1>
+      <h2>Example: {calculate(exampleData, true)}</h2>
+      <p>Part 2</p>
+      <h1>Answer: {calculate(data, false)}</h1>
+      <h2>Example: {calculate(exampleData, false)}</h2>
+    </div>
+  );
+}
+
+export default DayTwentyone;
