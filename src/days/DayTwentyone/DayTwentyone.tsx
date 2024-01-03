2c7bc481abad1c17984766dbdad2fbcf28c402e7
Sebastian-Rosenblad <sebastian.a.rosenblad@gmail.com>
2024-01-03 12:15:42 +0100

    Day 21 code not finished

--- src/days/DayTwentyone/DayTwentyone.tsx
+++ src/days/DayTwentyone/DayTwentyone.tsx
@@ -6,12 +6,17 @@ interface CoordsM {
   x: number;
   y: number;
 }
+interface RowBoundM {
+  y: number;
+  min: number;
+  max: number;
+}
 
 function DayTwentyone() {
   const data: Array<string> = dayTwentyoneData.split(/\r?\n/);
   const exampleData: Array<string> = dayTwentyoneExample.split(/\r?\n/);
-  let dictionary: { [key: string]: Array<CoordsM>; };
   let width: number, height: number;
+  let bounds: Array<RowBoundM>
 
   /**
    * Part 1: time 00:00:00 - rank 0000
@@ -20,30 +25,55 @@ function DayTwentyone() {
 
   function calculate(a: Array<string>, partOne: boolean, steps: number): string {
     let time: number = Date.now();
-    let reachable: Array<CoordsM> = [FindStart(a)];
+    const unreachables: Array<CoordsM> = a.length < 20 ? [{x:8,y:8}] : [{x:125,y:8},{x:124,y:38},{x:27,y:63},{x:107,y:108},{x:116,y:127},{x:117,y:127}];
+    let checked: Array<CoordsM> = [];
+    const start: CoordsM = FindStart(a);
+    bounds = [{ y: start.y, min: start.x, max: start.x }];
+    let positions: Array<CoordsM> = [start];
+    let possible: Array<number> = [1, 0];
+    let step: number = 0;
     const map: Array<Array<string>> = transpose(a.map(line => line.split("")));
-    dictionary = {};
     width = map.length; height = map[0].length;
-    for (let i = 0; i < steps; i++) {
-      let key: string = ToKey(reachable);
-      if (dictionary.hasOwnProperty(key)) {
-        reachable = dictionary[key];
-        continue;
-      }
-      dictionary[key] = reachable;
-      let newReach: Array<CoordsM> = [];
-      while (reachable.length > 0) {
-        let position: CoordsM | undefined = reachable.shift();
-        if (position !== undefined) {
-          if (IsValid(position.x - 1, position.y, newReach, map)) newReach.push({ x: position.x - 1, y: position.y });
-          if (IsValid(position.x + 1, position.y, newReach, map)) newReach.push({ x: position.x + 1, y: position.y });
-          if (IsValid(position.x, position.y - 1, newReach, map)) newReach.push({ x: position.x, y: position.y - 1 });
-          if (IsValid(position.x, position.y + 1, newReach, map)) newReach.push({ x: position.x, y: position.y + 1 });
-        }
-      }
-      reachable = newReach;
+    let changes: Array<number> = [1];
+    while (step < steps) {
+      step += 1;
+      let check: Array<CoordsM> = [...positions];
+      positions = [];
+      check.forEach(coord => {
+        let dirs: Array<CoordsM> = [
+          { x: coord.x + 1, y: coord.y },
+          { x: coord.x - 1, y: coord.y },
+          { x: coord.x, y: coord.y + 1 },
+          { x: coord.x, y: coord.y - 1 }
+        ];
+        dirs.forEach(dir => {
+          if (!InsideBounds(dir)) {
+            if (IsValid(dir.x, dir.y, checked, map, partOne)) positions.push(dir);
+            checked.push(dir);
+          }
+        });
+        checked.sort((a, b) => a.y === b.y ? (a.x < start.x ? b.x - a.x : a.x - b.x) : a.y - b.y).forEach(check => {
+          let row: RowBoundM | undefined = bounds.find(bound => bound.y === check.y);
+          if (!row) bounds.push({ y: check.y, min: check.x, max: check.x });
+          else {
+            if (row.min === check.x + 1) row.min -= 1;
+            if (row.max === check.x - 1) row.max += 1;
+          }
+        });
+        unreachables.forEach(unreachable => {
+          let rows: Array<RowBoundM> = bounds.filter(bound => bound.y % height + (bound.y % height < 0 ? height : 0) === unreachable.y);
+          rows.forEach(row => {
+            if (height + row.min % height === unreachable.x + 1) row.min -= 1;
+            if (row.max % height === unreachable.x - 1) row.max += 1;
+          });
+        });
+        checked = checked.filter(c => !InsideBounds(c));
+      });
+      possible[step % 2] += positions.length;
+      changes.push(positions.length);
     }
-    return reachable.length + " (" + (Date.now() - time) + ")";
+    console.log(changes.map((c, i) => c - i * 4));
+    return possible[step % 2] + " (" + (Date.now() - time) + ")";
   }
   function FindStart(a: Array<string>): CoordsM {
     for (let i = 0; i < a.length; i++) {
@@ -51,14 +81,16 @@ function DayTwentyone() {
     }
     return { x: 0, y: 0 };
   }
-  function ToKey(coords: Array<CoordsM>): string {
-    return coords.sort((a, b) => a.x !== b.x ? a.x - b.x : a.y - b.y).map(coord => coord.x + "," + coord.y).join("-");
-  }
-  function IsValid(x: number, y: number, positions: Array<CoordsM>, map: Array<Array<string>>): boolean {
+  function IsValid(x: number, y: number, positions: Array<CoordsM>, map: Array<Array<string>>, partOne: boolean): boolean {
     if (positions.find(position => position.x === x && position.y === y)) return false;
-    if (x < 0 || x >= width || y < 0 || y >= height) return false;
-    if (map[x][y] === "#") return false;
+    if (partOne && (x < 0 || x >= width || y < 0 || y >= height)) return false;
+    let cx: number = x % width, cy: number = y % height;
+    if (map[cx < 0 ? cx + width : cx][cy < 0 ? cy + height : cy] === "#") return false;
     return true;
+  }
+  function InsideBounds(coord: CoordsM): boolean {
+    const row: RowBoundM | undefined = bounds.find(bound => bound.y === coord.y);
+    return row !== undefined && (coord.x >= row.min && coord.x <= row.max);
   }
 
   return (
@@ -68,7 +100,12 @@ function DayTwentyone() {
       <h2>Example: {calculate(exampleData, true, 6)}</h2>
       <p>Part 2</p>
       <h1>Answer: {calculate(data, false, 0)}</h1>
-      <h2>Example: {calculate(exampleData, false, 0)}</h2>
+      <h2>Example: {calculate(exampleData, false, 6)}</h2>
+      <h2>Example: {calculate(exampleData, false, 10)}</h2>
+      <h2>Example: {calculate(exampleData, false, 50)}</h2>
+      <h2>Example: {calculate(exampleData, false, 100)}</h2>
+      <h2>Example: {calculate(exampleData, false, 65+131*2)}</h2>
+      <h2>Example: {calculate(exampleData, false, 500)}</h2>
     </div>
   );
 }
